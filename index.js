d5b7672a465d53cb37c65fac146c2ebd099ae144
ferinsh <ferinsharaf23@gmail.com>
2023-10-22 03:08:43 +0530

    ppppppp

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const div = document.createElement('div');
-//creates a new div referenced in the variable 'div'
-div.style.color = 'blue';
+//
+//const mainBox = document.querySelector('div.main-box')
+//const body = document.querySelector('body');
+//console.log(mainBox);
+//
+//
+//
+//
+//div = document.createElement('div');
+//
+//
+//
+//div.textContent = 'Hello World';
+//
+//
+//
+//
+//
+//console.log(div)
+//console.log(body)
+//mainBox.appendChild(div)
+//
